0958b0f2c9bffb7c26848bfaa13c98802439352e
Anush-Raghavender <64842234+Anush-Raghavender@users.noreply.github.com>
2021-07-22 18:02:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,13 @@
 /*----- Collection -----*/
-function viewDetails1() {
-    var vd = document.getElementById("view-details-1");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-1").innerHTML = "Show Less";
+function viewDetails(watchdiv, watchbtn) {
+    var wd = document.getElementById(watchdiv);
+    var wb = document.getElementById(watchbtn);
+    if (wd.style.display === "none") {
+      wd.style.display = "block";
+      wb.innerHTML = "Show Less";
     } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-1").innerHTML = "View Details";
+      wd.style.display = "none";
+      wb.innerHTML = "View Details";
     }
 }
-function viewDetails2() {
-    var vd = document.getElementById("view-details-2");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-2").innerHTML = "Show Less";
-    } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-2").innerHTML = "View Details";
-    }
-}
-function viewDetails3() {
-    var vd = document.getElementById("view-details-3");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-3").innerHTML = "Show Less";
-    } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-3").innerHTML = "View Details";
-    }
-}
-function viewDetails4() {
-    var vd = document.getElementById("view-details-4");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-4").innerHTML = "Show Less";
-    } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-4").innerHTML = "View Details";
-    }
-}
-function viewDetails5() {
-    var vd = document.getElementById("view-details-5");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-5").innerHTML = "Show Less";
-    } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-5").innerHTML = "View Details";
-    }
-}
-function viewDetails6() {
-    var vd = document.getElementById("view-details-6");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-6").innerHTML = "Show Less";
-    } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-6").innerHTML = "View Details";
-    }
-}
-function viewDetails7() {
-    var vd = document.getElementById("view-details-7");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-7").innerHTML = "Show Less";
-    } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-7").innerHTML = "View Details";
-    }
-}
-function viewDetails8() {
-    var vd = document.getElementById("view-details-8");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-8").innerHTML = "Show Less";
-    } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-8").innerHTML = "View Details";
-    }
-}
-function viewDetails9() {
-    var vd = document.getElementById("view-details-9");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-9").innerHTML = "Show Less";
-    } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-9").innerHTML = "View Details";
-    }
-}
-function viewDetails10() {
-    var vd = document.getElementById("view-details-10");
-    if (vd.style.display === "none") {
-      vd.style.display = "block";
-      document.getElementById("view-details-button-10").innerHTML = "Show Less";
-    } else {
-      vd.style.display = "none";
-      document.getElementById("view-details-button-10").innerHTML = "View Details";
-    }
-}
+
